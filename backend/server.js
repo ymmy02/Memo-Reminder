d60b5505769990ce7908b1f1de6b7a7f4b3bcb17
Kohei Yamamoto <kh2yam1@gmial.com>
2019-07-05 16:06:14 -0400

    Implement some backend functions

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const router = express.Router();
+const memoRouter = express.Router();
 const PORT = 4000;
+
+const Memo = require('./model');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -14,6 +16,58 @@ const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection estublished successfully");
 });
+
+// TODO: Implementation
+memoRouter.route('/random').get((req, res) => {
+    console.log("Memo Reminder Application...");
+    res.status(200).send("Memo Reminder Application...");
+});
+
+memoRouter.route('/list').get((req, res) => {
+    Memo.find((err, memos) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(memos);
+        }
+    });
+});
+
+memoRouter.route('/:id').get((req, res) => {
+    const id = req.params.id;
+    Memo.findById(id, (err, memo) => {
+        if (err) {
+            res.status(400).send("find memo failed");
+        } else {
+            res.json(memo);
+        }
+    })
+});
+
+memoRouter.route('/add').post((req, res) => {
+    let memo = new Memo(req.body);
+    memo.save()
+        .then(memo => {
+            res.status(200).json({'memo': "memo added successfully"});
+        })
+        .catch(err => {
+            res.status(400).send("adding new memo failed")
+        });
+});
+
+// TODO: Implementation
+memoRouter.route('/update/:id').post((req, res) => {
+    console.log("Memo Reminder Application...");
+    res.status(200).send("Memo Reminder Application...");
+});
+
+// TODO: Implementation
+memoRouter.route('/delete/:id').post((req, res) => {
+    console.log("Memo Reminder Application...");
+    res.status(200).send("Memo Reminder Application...");
+});
+
+app.use('/memos', memoRouter);
 
 app.listen(PORT, function() {
     console.log("Server is ready...");
