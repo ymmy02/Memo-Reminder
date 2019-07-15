9b4467aee4ec57054c1e7284fc9a6a57d259796f
Kohei Yamamoto <kh2yam1@gmial.com>
2019-07-14 21:58:36 -0400

    Add header

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="header">
+        <h1>Memo Reminder</h1>
+    </header>
+</template>
+
+<script>
+export default {
+   name: 'Header'
+}
+</script>
+
+<style scoped>
+.header {
+    padding: 20px 0 20px 0;
+    background-color: #393f4c;
+    color:ivory;
+    text-align: center;
+}
+</style>
+
