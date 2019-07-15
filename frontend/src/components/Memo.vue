c1fdbd177042de0696b412fce4351aa3713f5c8a
Kohei Yamamoto <kh2yam1@gmial.com>
2019-07-15 11:54:30 -0400

    Implement Memo component

---
+++ frontend/src/components/Memo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="memo-content">
+        {{ memo.memo_content }}
+        <b-button variant="outline-success" class="edit-button">Edit</b-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Memo",
+    props: ["memo"],
+}
+</script>
+
+<style scoped>
+.memo-content {
+    padding: 10px 0 10px 0;
+}
+
+.edit-button {
+    float: right;
+}
+</style>
--- frontend/src/views/MemoList.vue
+++ frontend/src/views/MemoList.vue
@@ -1,6 +1,37 @@
 <template>
-  <div class="memo-list">
-    <b-button variant="outline-primary">Button</b-button>
+  <div class="memo-list container">
+    <div v-for="memo in memolist" :key="memo._id">
+      <Memo :memo="memo" />
+    </div>
   </div>
 </template>
+
+<script>
+import Memo from '../components/Memo.vue';
+import { apiUrl } from '../variables';
+
+export default {
+  components: {
+    Memo
+  },
+  data() {
+    return {
+      memolist: [
+        {
+          _id: '1',
+          memo_content: 'First memo'
+        },
+        {
+          _id: '2',
+          memo_content: 'Second memo'
+        },
+        {
+          _id: '3',
+          memo_content: 'Third memo'
+        },
+      ]
+    }
+  }
+}
+</script>
 
